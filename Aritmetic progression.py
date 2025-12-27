8c7c6a9db1c1d5432552a9eb9bc494da7d39defa
Cauã Grimaldi <cauagrimaldi2008@gmail.com>
2025-12-27 15:04:52 -0300

    Aritmetic Progression

---
+++ Aritmetic progression.py
@@ -0,0 +1,11 @@
+print('AP Generator')
+print('-='*12)
+first=int(input('Type the first term:'))
+ratio=int(input('Ratio of the AP:'))
+term=first
+counter=1
+while counter <=10:
+    print(f'{term} -->',end='')
+    termo+=ratio
+    counter+=1
+print('END')
